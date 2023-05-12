9f6535dffd8d0ca91331a53da41d5d86030b19ea
Tom OMara <tomara3@gatech.edu>
2023-05-12 22:44:54 +1000

    Add playing horizontal and verical movement, and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRb;
+    private float horizontalInput;
+    private float verticalInput;
+    public float jumpForce = 50;
+    float speed = 8.0f;
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (Input.GetButtonDown("up"))
+        {
+
+        }
+
+        if (Input.GetButtonDown("down"))
+        {
+
+        }
+        if (Input.GetButtonDown("left"))
+        {
+
+        }
+        if (Input.GetButtonDown("right"))
+        {
+
+        }
+        if (Input.GetButtonDown("jump"))
+        {
+
+        }
+        */
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
     }
 }
